a06da141b1450434adcbf065367d8a1b6a10d688
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-06-01 12:05:05 -0500

    uglify copy config

--- config/copy.config.js
+++ config/copy.config.js
@@ -2,24 +2,12 @@
 // provide a name for an entry, it can be anything such as 'copyAssets' or 'copyFonts'
 // then provide an object with a `src` array of globs and a `dest` string
 module.exports = {
-  copyAssets: {
-    src: ['{{SRC}}/assets/**/*'],
-    dest: '{{WWW}}/assets'
-  },
-  copyIndexContent: {
-    src: ['{{SRC}}/index.html', '{{SRC}}/manifest.json', '{{SRC}}/service-worker.js'],
-    dest: '{{WWW}}'
-  },
-  copyFonts: {
-    src: ['{{ROOT}}/node_modules/ionicons/dist/fonts/**/*', '{{ROOT}}/node_modules/ionic-angular/fonts/**/*'],
-    dest: '{{WWW}}/assets/fonts'
-  },
   copyPolyfills: {
     src: ['{{ROOT}}/hackz/zone-only-polyfill.js'],
     dest: '{{BUILD}}'
   },
-  copySwToolbox: {
-    src: ['{{ROOT}}/node_modules/sw-toolbox/sw-toolbox.js'],
-    dest: '{{BUILD}}'
+  uglifyHax: {
+    src: ['{{ROOT}}/hackz/uglifyjs.js'],
+    dest: `{{ROOT}}/node_modules/@ionic/app-scripts/dist/uglifyjs.js`
   }
 }
