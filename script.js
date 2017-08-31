38a655de335c022688768fb4699a15a26bee9c42
Dan Bucholtz <dan.bucholtz@gmail.com>
2017-08-31 17:01:49 -0500

    wip

---
+++ script.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+
+var SourceMapConsumer = require('source-map').SourceMapConsumer;
+
+var sourceMapData = fs.readFileSync('www/build/main.js.map').toString('utf-8');
+
+var smc = new SourceMapConsumer(sourceMapData);
+
+let tsReg = /\.ts/ig;
+
+let original = smc.originalPositionFor({
+  line: 30,
+  column: 9
+});
+
+console.log('original: ', original);
+
+let sourceContent = smc.sourceContentFor(original.source);
+
+let lines = sourceContent.split('\n');
+
+let center = lines[original.line];
+
+let context = lines.slice(Math.max(0, original.line - 4), Math.min(lines.length, original.line + 4))
+
+console.log(context.join('\n'));
